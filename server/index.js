6cebc49cd5d3d8f6318452b77d94b115296a21f2
sabrinawhite4 <sabrinawhite4@me.com>
2021-06-24 13:48:51 -0600

    initial commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 app.get('/', (req, res) => {
 
-    res.sendFile( path.join(__filename, '../index.html') );
+    res.sendFile( path.join(__dirname, '../index.html') );
 
 });
 
